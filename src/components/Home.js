c1c3463c7b8710a632cba39afc89c0ce489c45ce
HehNice <122663831+HehNiceArt@users.noreply.github.com>
2024-09-18 17:19:34 +0800

    home

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Home.css'
+export default function Home() {
+    return (
+        <div className="Home-Container"  >
+            <div className="Home-Hero">
+                <div className="Home-Content">
+                    <h1>Welcome</h1>
+                </div>
+            </div>
+            <div className="Home-Details">
+                <div className="Home-Summary">
+                </div>
+                <div className="Home-Text">
+                    <h1>Welcome to My Site</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
